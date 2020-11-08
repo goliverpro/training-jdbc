35f5d25500fbaded646091302d016827568e4345
Gabriel Oliveira <gosilva.pro@gmail.com>
2020-11-08 12:30:19 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,16 +24,22 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		System.out.println("\n=== TEST 3: Seller findAll=====");
+		System.out.println("\n=== TEST 3: Seller findAll =====");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TEST 4: Seller insert=====");
+		System.out.println("\n=== TEST 4: Seller insert =====");
 		Seller newSeller = new Seller (null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: Seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
